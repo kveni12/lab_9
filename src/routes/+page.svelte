5590ac4a97dfcef7a18593301151c8e9e29c0efe
Krishna <kveni@mit.edu>
2026-04-09 15:55:05 -0400

    bluebikes stations

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,13 +1,27 @@
 <script>
 	import { PUBLIC_MAPBOX_ACCESS_TOKEN } from '$env/static/public';
 	import { onMount } from 'svelte';
+	import * as d3 from 'd3';
 	import mapboxgl from 'mapbox-gl';
 	import '../../node_modules/mapbox-gl/dist/mapbox-gl.css';
 
 	mapboxgl.accessToken = PUBLIC_MAPBOX_ACCESS_TOKEN;
 
+	/** @typedef {{ Number: string, NAME: string, Lat: string | number, Long: string | number, 'Seasonal Status': string, Municipality: string, 'Total Docks': string | number }} Station */
+
+	/** @type {Station[]} */
+	let stations = $state([]);
+	/** @type {import('mapbox-gl').Map | undefined} */
+	let map = $state();
+	let mapViewChanged = $state(0);
+
+	async function loadStations() {
+		stations = await d3.csv('https://vis-society.github.io/labs/9/data/bluebikes-stations.csv');
+		console.log('Bluebikes stations:', stations);
+	}
+
 	async function initializeMap() {
-		const map = new mapboxgl.Map({
+		const mapInstance = new mapboxgl.Map({
 			container: 'map',
 			style: 'mapbox://styles/mapbox/streets-v12',
 			center: [-71.1097, 42.3736],
@@ -15,15 +29,16 @@
 			minZoom: 10,
 			maxZoom: 18
 		});
+		map = mapInstance;
 
-		await new Promise((resolve) => map.on('load', resolve));
+		await new Promise((resolve) => mapInstance.on('load', resolve));
 
-		map.addSource('boston_route', {
+		mapInstance.addSource('boston_route', {
 			type: 'geojson',
 			data: 'https://bostonopendata-boston.opendata.arcgis.com/datasets/boston::existing-bike-network-2022.geojson?outSR=%7B%22latestWkid%22%3A3857%2C%22wkid%22%3A102100%7D'
 		});
 
-		map.addLayer({
+		mapInstance.addLayer({
 			id: 'boston-bike-lanes',
 			type: 'line',
 			source: 'boston_route',
@@ -34,12 +49,12 @@
 			}
 		});
 
-		map.addSource('cambridge_route', {
+		mapInstance.addSource('cambridge_route', {
 			type: 'geojson',
 			data: 'https://raw.githubusercontent.com/cambridgegis/cambridgegis_data/main/Recreation/Bike_Facilities/RECREATION_BikeFacilities.geojson'
 		});
 
-		map.addLayer({
+		mapInstance.addLayer({
 			id: 'cambridge-bike-lanes',
 			type: 'line',
 			source: 'cambridge_route',
@@ -50,16 +65,23 @@
 			}
 		});
 
-		return map;
+		mapInstance.on('move', () => {
+			mapViewChanged += 1;
+		});
+	}
+
+	/** @param {Station} station */
+	function getCoords(station) {
+		if (!map) return { cx: 0, cy: 0 };
+
+		const point = new mapboxgl.LngLat(+station.Long, +station.Lat);
+		const { x, y } = map.project(point);
+		return { cx: x, cy: y };
 	}
 
 	onMount(() => {
-		/** @type {import('mapbox-gl').Map | undefined} */
-		let map;
-
-		initializeMap().then((createdMap) => {
-			map = createdMap;
-		});
+		loadStations();
+		initializeMap();
 
 		return () => map?.remove();
 	});
@@ -79,7 +101,17 @@
 	stories about how people move through the city.
 </p>
 
-<div id="map"></div>
+<div id="map">
+	<svg>
+		{#if map}
+			{#key mapViewChanged}
+				{#each stations as station}
+					<circle {...getCoords(station)} r="5" fill="steelblue" stroke="white" stroke-width="1" />
+				{/each}
+			{/key}
+		{/if}
+	</svg>
+</div>
 
 <style>
 	@import url("$lib/global.css");
@@ -89,5 +121,15 @@
 		background: #d8f0ff;
 		min-height: 24rem;
 		border-radius: 1rem;
+		position: relative;
+		overflow: hidden;
+	}
+
+	#map svg {
+		position: absolute;
+		z-index: 1;
+		width: 100%;
+		height: 100%;
+		pointer-events: none;
 	}
 </style>
